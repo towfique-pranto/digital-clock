ff1161d6e0976d79dc5abb1b0150546c408e08b5
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-16 01:18:19 +0600

    added logic for the clock. changed background color, font size and margin

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(intervalId);
+      console.log("Interval cleared");
+    };
+  }, []);
+
+  function formatTime() {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    return `${doubleDigits(hours)}:${doubleDigits(minutes)}:${doubleDigits(seconds)}`;
+  }
+  function doubleDigits(time) {
+    return (time < 10 ? "0" : "") + time;
+  }
+
   return (
     <div className="container">
       <div className="clock">
-        <span>00:00</span>
+        <span>{formatTime()}</span>
       </div>
     </div>
   );
