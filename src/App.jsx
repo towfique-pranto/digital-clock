b9317f4858f574ff5eb2665e05c80357b802e42f
Towfique Kabir <towfique.pranto@gmail.com>
2026-03-16 00:21:22 +0600

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import DigitalClock from "./DigitalClock";
 
 function App() {
-  return <></>;
+  return <DigitalClock />;
 }
 
 export default App;
---
+++ src/DigitalClock.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function DigitalClock() {
+  return (
+    <div className="container">
+      <div className="clock">
+        <span>00:00</span>
+      </div>
+    </div>
+  );
+}
+
+export default DigitalClock;
